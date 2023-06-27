ca18e6a6dce8eaafd81b5eef622961aba3d06583
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2023-06-27 20:46:12 -0300

    feat: adiciona envio de mensagem no bot ao digitar a senha3

--- src/controllers/TryController.ts
+++ src/controllers/TryController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import http from "http";
 
 export class TryController {
   async try(req: Request, res: Response) {
@@ -11,6 +12,14 @@ export class TryController {
       return res.status(200).json({ url: "7f69-aaa4-7054-4ee6" });
     }
     if (tryValue === "senha3") {
+      const options = {
+        hostname: "localhost",
+        port: 3001,
+        path: "/puzzle",
+      };
+
+      const request = http.get(options, (response) => {});
+
       return res.status(200).json({ url: "7d14-3c68-ef0a-9ebf" });
     }
     if (tryValue === "senha4") {
