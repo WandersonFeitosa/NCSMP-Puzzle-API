f92203de11e8a3094bc24f4888e098b5231cc1ad
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2023-06-30 23:39:51 -0300

    feat: adiciona o uso do .env

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import express from "express";
 import routes from "./routes/routes";
+import dotenv from "dotenv";
+import path from "path";
 
-const path = require("path");
+dotenv.config();
+
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
 app.use(routes);
 
-// Middleware to serve files from the "public" folder
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
