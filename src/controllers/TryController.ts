0a6b5774e15b1a4f59b48aeb5324d6092564ae8b
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2023-07-01 10:28:36 -0300

    feat: adiciona verificação de etapas nos enigmas

--- src/controllers/TryController.ts
+++ src/controllers/TryController.ts
@@ -1,55 +1,176 @@
 import { Request, Response } from "express";
-import http from "http";
+import mongoose from "mongoose";
+import axios from "axios";
+
+const logTrysSchema = new mongoose.Schema({
+  phase1: Boolean,
+  phase2: Boolean,
+  phase3: Boolean,
+  phase4: Boolean,
+  phase5: Boolean,
+  phase6: Boolean,
+  phase7: Boolean,
+  phase8: Boolean,
+  phase9: Boolean,
+  phase10: Boolean,
+  phase11: Boolean,
+  phase12: Boolean,
+});
+
+const LogTrys = mongoose.model("LogTrys", logTrysSchema);
+
+function sendKop(message: string) {
+  const botUrl = "http://localhost:3001/puzzle";
+  const postData = { message };
+  axios
+    .post(botUrl, postData)
+    .then((response) => {
+      console.log("Response:", response.data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
 
 export class TryController {
   async try(req: Request, res: Response) {
     const { tryValue } = req.body;
 
+    // FASE 1
     if (tryValue === "WORKING") {
+      const nextPhaseStatus = await LogTrys.findOne({ phase2: false });
+
+      if (nextPhaseStatus) {
+        await LogTrys.updateOne({ phase2: false }, { phase2: true });
+        sendKop("ENIGMA 1 RESOLVIDO");
+      }
+
       return res.status(200).json({ url: "fdcd-bd80-8202-9905" });
     }
+
+    // FASE 2
     if (tryValue === "EU PRECISO ME LEMBRAR DA MINHA HISTORIA") {
+      const phaseStatus = await LogTrys.findOne({ phase2: false });
+      const nextPhaseStatus = await LogTrys.findOne({ phase3: false });
+      if (phaseStatus) {
+        return res.status(400).json({ error: "Tente Novamente" });
+      }
+
+      if (nextPhaseStatus) {
+        await LogTrys.updateOne({ phase3: false }, { phase3: true });
+        sendKop("ENIGMA 2 RESOLVIDO");
+      }
+
       return res.status(200).json({ url: "7f69-aaa4-7054-4ee6" });
     }
+
+    // FASE 3
     if (tryValue === "senha3") {
-      // const options = {
-      //   hostname: "localhost",
-      //   port: 3001,
-      //   path: "/puzzle",
-      // };
-      // try {
-      //   const request = http.get(options, (response) => {});
-      // } catch (err) {
-      //   console.log(err);
-      // }
+      const phaseStatus = await LogTrys.findOne({ phase3: false });
+      const nextPhaseStatus = await LogTrys.findOne({ phase4: false });
+      if (phaseStatus) {
+        return res.status(400).json({ error: "Tente Novamente" });
+      }
+
+      if (nextPhaseStatus) {
+        await LogTrys.updateOne({ phase4: false }, { phase4: true });
+        sendKop("ENIGMA 3 RESOLVIDO");
+      }
+
       return res.status(200).json({ url: "7d14-3c68-ef0a-9ebf" });
     }
+
+    // FASE 4
     if (tryValue === "senha4") {
+      const phaseStatus = await LogTrys.findOne({ phase4: false });
+      const nextPhaseStatus = await LogTrys.findOne({ phase5: false });
+      if (phaseStatus) {
+        return res.status(400).json({ error: "Tente Novamente" });
+      }
+
+      if (nextPhaseStatus) {
+        await LogTrys.updateOne({ phase5: false }, { phase5: true });
+        sendKop("ENIGMA 4 RESOLVIDO");
+      }
+
       return res.status(200).json({ url: "nao-ta-aqui" });
     }
-    if (tryValue === "sculk") {
+
+    // FASE 5
+    if (tryValue === "SCULK") {
+      const phaseStatus = await LogTrys.findOne({ phase5: false });
+      const nextPhaseStatus = await LogTrys.findOne({ phase6: false });
+      if (phaseStatus) {
+        return res.status(400).json({ error: "Tente Novamente" });
+      }
+
+      if (nextPhaseStatus) {
+        await LogTrys.updateOne({ phase6: false }, { phase6: true });
+        sendKop("ENIGMA 5 RESOLVIDO");
+      }
       return res.status(200).json({ url: "1439-1d71-ecb8-d4fd" });
     }
-    if (tryValue === "senha6") {
-      return res.status(200).json({ url: "1439-1d71-ecb8-d4fc" });
-    }
+
+    // FASE 6
     if (tryValue === "LIBERTE") {
+      const phaseStatus = await LogTrys.findOne({ phase6: false });
+      const nextPhaseStatus = await LogTrys.findOne({ phase7: false });
+      if (phaseStatus) {
+        return res.status(400).json({ error: "Tente Novamente" });
+      }
+
+      if (nextPhaseStatus) {
+        await LogTrys.updateOne({ phase7: false }, { phase7: true });
+        sendKop("ENIGMA 6 RESOLVIDO");
+      }
+
       return res.status(200).json({ url: "1439-1d71-ecb8-d4fg" });
     }
-    if (tryValue === "ajuda") {
-      return res
-        .status(200)
-        .json({
-          url: "Kr4d105s25p5Rn5f46d17s2Ar3d64s2Xe4f145d66s2Kr4d75s1Ar3d54s2Ne3s23p2Ar4s2Ar3d104s24p3Rn5f146d107s27p6",
-        });
-    }
-    if (tryValue === "INFORMACAO") {
-      return res.status(200).json({ url: "1439-1d71-ecb8-d4fh" });
-    }
-    if (tryValue === "RAIVA") {
-      return res.status(200).json({ url: "1439-1d71-ecb8-d4fi" });
+
+    // FASE 7
+    if (tryValue === "REGRET") {
+      const phaseStatus = await LogTrys.findOne({ phase7: false });
+      const nextPhaseStatus = await LogTrys.findOne({ phase8: false });
+      if (phaseStatus) {
+        return res.status(400).json({ error: "Tente Novamente" });
+      }
+
+      if (nextPhaseStatus) {
+        await LogTrys.updateOne({ phase8: false }, { phase8: true });
+        sendKop("ENIGMA 7 RESOLVIDO");
+      }
+
+      return res.status(200).json({
+        url: "Kr4d105s25p5Rn5f46d17s2Ar3d64s2Xe4f145d66s2Kr4d75s1Ar3d54s2Ne3s23p2Ar4s2Ar3d104s24p3Rn5f146d107s27p6",
+      });
     }
 
-    return res.status(400).json({ error: "Tente Novamente" });
+    // FASE 7
+    if (tryValue === "INFORMACAO") {
+      const phaseStatus = await LogTrys.findOne({ phase8: false });
+      const nextPhaseStatus = await LogTrys.findOne({ phase9: false });
+      if (phaseStatus) {
+        return res.status(400).json({ error: "Tente Novamente" });
+      }
+
+      if (nextPhaseStatus) {
+        await LogTrys.updateOne({ phase9: false }, { phase9: true });
+        sendKop("ENIGMA 8 RESOLVIDO");
+      }
+
+      return res.status(200).json({ url: "1439-1d71-ecb8-d4fh" });
+    }
+
+    // FASE 9
+    if (tryValue === "RAIVA") {
+      const phaseStatus = await LogTrys.findOne({ phase9: false });
+
+      if (phaseStatus) {
+        return res.status(400).json({ error: "Tente Novamente" });
+      }
+
+      sendKop("ENIGMA 9 RESOLVIDO");
+      return res.status(400).json({ error: "Tente Novamente" });
+    }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import routes from "./routes/routes";
 import dotenv from "dotenv";
 import path from "path";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
+const dbURI = process.env.MONGODB_URI || "";
 
 const app = express();
 app.use(express.json());
@@ -13,6 +15,17 @@ app.use(routes);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+function startServer() {
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}
+mongoose
+  .connect(dbURI)
+  .then(() => {
+    console.log("Conectado ao banco de dados");
+    startServer();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
