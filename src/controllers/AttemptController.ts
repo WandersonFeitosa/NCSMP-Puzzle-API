66fff921db7b0bfa12e4c3cd9f1cdec22208b04b
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2023-07-08 01:39:35 -0300

    recfator: recria a função de tentativa

---
+++ src/controllers/AttemptController.ts
@@ -0,0 +1,87 @@
+import axios from "axios";
+import { Request, Response } from "express";
+import mongoose from "mongoose";
+
+const AttemptsTrySchema = new mongoose.Schema({
+  phase: Number,
+  passwords: [String],
+  completionDate: Date,
+  completed: Boolean,
+  unlocked: Boolean,
+  url: String,
+});
+
+interface AttemptTrys extends mongoose.Document {
+  phase: number;
+  passwords: string[];
+  completionDate: Date;
+  completed: boolean;
+  unlocked: boolean;
+  url: string;
+}
+
+const AttemptTrys = mongoose.model("AttemptsTrys", AttemptsTrySchema);
+
+export class AttemptController {
+  async attempt(req: Request, res: Response) {
+    const { tryValue } = req.body;
+
+
+    const passwordArray = [tryValue];
+    const phase = await AttemptTrys.findOne({ passwords: passwordArray[0] });
+    const phaseNumber = phase?.phase;
+    const nextPhaseNumber = phaseNumber ? phaseNumber + 1 : 1;
+    const nextPhase = await AttemptTrys.findOne({
+      phase: nextPhaseNumber,
+    });
+    const nextPhaseUrl = nextPhase?.url;
+
+    async function uptadatePhases() {
+      if (!phase?.completed) {
+        await AttemptTrys.updateOne(
+          { phase: phaseNumber },
+          { completed: true, completionDate: new Date() }
+        );
+        await AttemptTrys.updateOne(
+          { phase: nextPhaseNumber },
+          { unlocked: true }
+        );
+      }
+    }
+
+    async function sendKop(message: string) {
+      if (!phase?.completed) {
+        const botUrl = process.env.BOT_URL || "http://localhost:3001/puzzle";
+        const postData = { message };
+        axios
+          .post(botUrl, postData)
+          .then((response) => {
+            console.log("Response:", response.data);
+          })
+          .catch((error) => {
+            console.log("O bot deve estar offline \n", message);
+          });
+      }
+    }
+
+    if (phase && phase.unlocked) {
+      sendKop(`Enigma ${phaseNumber} resolvido`);
+      uptadatePhases();
+      return res.status(200).json({ url: nextPhaseUrl });
+    }
+
+    res.status(400).json({ error: "Tente Novamente" });
+  }
+
+  async getCompletionDate(req: Request, res: Response) {
+    const { phase } = req.body;
+    const phaseObject = await AttemptTrys.findOne({ phase, completed: true });
+    const completionDate = phaseObject?.completionDate;
+
+    if (completionDate) {
+      return res.status(200).json({ completionDate });
+    }
+
+    return res.status(400).json({ error: "Ainda não foi completado" });
+  }
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import cors from "cors";
-import { TryController } from "../controllers/TryController";
+import { AttemptController } from "../controllers/AttemptController";
 
 const routes = Router();
 
 routes.use(cors());
 
-routes.post("/try", new TryController().try);
-routes.get("/getDate", new TryController().getCompletionDate)
+routes.post("/try", new AttemptController().attempt);
+routes.get("/getDate", new AttemptController().getCompletionDate)
 
 export default routes;
