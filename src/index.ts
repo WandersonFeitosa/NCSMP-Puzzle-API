b8d5d936fc4a350b47abde4f258e9b440419a033
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2023-07-08 01:42:35 -0300

    chore: prepara arquivos para deploy no Render

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.use(express.static(path.join(__dirname, "public")));
 
 function startServer() {
   app.listen(port, () => {
